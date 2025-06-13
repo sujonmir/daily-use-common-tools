9d24a34d05fbe890577aea734241d0d0a8eeacbf
sujonmir <sujonmhk786@gmail.com>
2025-06-14 01:55:51 +0600

    reduce symbols <>

--- js/password.js
+++ js/password.js
@@ -72,7 +72,7 @@ function getRandomNumber() {
 }
 
 function getRandomSymbol() {
-  const symbols = `~.!@#$%^&*<>`;
+  const symbols = `~.!@#$%^&*`;
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
