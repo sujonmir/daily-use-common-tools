874cdcd1002102e4d88cecf730c07cc74638ae22
sujonmir <sujonmhk786@gmail.com>
2026-04-19 10:23:56 +0600

    mdtoHTML functionality fix

--- js/shortcut.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('keydown', function(e) {
-    if (e.ctrlKey && e.key === 'f') {
-        e.preventDefault();
-        document.getElementById('search-input').focus();
-    }
-});
